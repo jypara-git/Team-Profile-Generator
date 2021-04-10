2538cd263213b0bbc727cc41ff36a17ef462de90
jypara-git <zhyparaabdieva@gmail.com>
2021-04-09 23:38:27 -0700

    adding prompts for engineer

--- index.js
+++ index.js
@@ -60,25 +60,84 @@ const promptUser = () => {
             name: 'options',
             message: "If you would like to add more team members or finish choose from options below.",
             choices: ['Engineer', 'Intern', 'Finish'],
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a valid answer!');
-                    return false;
-                }
-            }
-            
         }
     ])
-}
+};
+
+const promptEngineer = () => {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is Engineer's name?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a valid answer!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is Engineer's employee ID?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a valid answer!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is Engineer's email address?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a valid answer!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'username',
+                message: "What is Engineer's GitHub username?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a valid answer!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'list',
+                name: 'options',
+                message: "If you would like to add more team members or finish choose from options below.",
+                choices: ['Engineer', 'Intern', 'Finish'],
+            }
+        ])
+        .then((engInfo) => {
+            console.log(engInfo);
+            if (engInfo.options === 'Engineer') {
+                promptEngineer();
+            }
+        }) 
+};
+
 promptUser()
     .then((userInput) => {
         console.log(userInput);
         const manager = new Manager(userInput.name, userInput.id, userInput.email, userInput.number);
-        console.log(manager.getName());
-        console.log(manager.getId());
-        console.log(manager.getEmail());
-        console.log(manager.getNumber());
-        console.log(manager.getRole());
+        if (userInput.options === 'Engineer') {
+            promptEngineer()
+        } 
     });
